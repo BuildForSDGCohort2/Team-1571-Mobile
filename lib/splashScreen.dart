2277524cbf1ff1529717dc46a1269419c5a16f6f
Udit Swaroopa <65483816+uditswaroopa@users.noreply.github.com>
2020-09-15 18:06:19 +0530

    Create splashScreen.dart

---
+++ lib/splashScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Center(
+        child: Text('SPLASH SCREEN',
+          style: TextStyle(
+            fontSize: 45,
+            fontWeight: FontWeight.w600,
+            color: Colors.redAccent[400],
+          ),
+        ),
+      ),
+    );
+  }
+}
